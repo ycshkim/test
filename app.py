17536ab1fd3963b51c63de8f7da7a65b6a6154de
ycshkim <26-3201@sc.gyo6.net>
2026-06-15 17:29:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(
-    page_title="문화재 훼손"
-)
-
 st.title("문화재 훼손 예측")
-st.divider()
 
-# 데이터 불러오기
 df = pd.read_csv("yc_heritage_detail_enriched.csv")
 
-st.subheader("시군구별 문화재 수")
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
 
-# 시군구별 문화재 개수 집계
-region_count = df["시군구명"].value_counts()
+    st.write(df.head())
 
-# 그래프 출력
-st.bar_chart(region_count)
+    region_count = df["시군구명"].value_counts()
+
+    st.subheader("시군구별 문화재 수")
+    st.bar_chart(region_count)
