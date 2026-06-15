a7c2c8af7bbaebcea5059a6412b92f8f543b8c8e
ycshkim <26-3201@sc.gyo6.net>
2026-06-15 17:16:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(
   page_title="문화재 훼손"
@@ -6,3 +7,6 @@ st.set_page_config(
 
 st.title("문화재 훼손 예측")
 st.divider()
+
+df=pd.read_csv("data/yc_heritage_detail_enriched.csv")
+st.dataframe(df)
