ccb9cd6aebc0d2c224c297a78267002f9aec1346
ycshkim <26-3201@sc.gyo6.net>
2026-06-20 09:44:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,11 @@ st.title("문화재 훼손 예측")
 st.divider()
 df=pd.read_csv("data/yc_heritage_detail_enriched.csv")
 st.dataframe(df)
+
+
+st.subheader("문화재 위치")
+
+map_df = df[["위도", "경도"]].dropna()
+map_df.columns = ["lat", "lon"]
+
+st.map(map_df)
