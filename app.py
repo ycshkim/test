8b2b9098fbcce939cbe2c3a04e322ee511ceaeb9
ycshkim <26-3201@sc.gyo6.net>
2026-06-20 09:43:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ st.set_page_config(
 )
 st.title("문화재 훼손 예측")
 st.divider()
-df=pd.read_csv("data/yc_heritage_detail_enruched.csv")
+df=pd.read_csv("data/yc_heritage_detail_enriched.csv")
 st.dataframe(df)
