1619fa5d40d526f1b0b88519285a3f2fda4065be
ycshkim <26-3201@sc.gyo6.net>
2026-06-22 15:52:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -45,17 +45,6 @@ st.plotly_chart(
 )
 
 
-import streamlit as st
-import pandas as pd
-
-st.title("📊 문화재 현황")
-
-df = pd.read_csv("data/yc_heritage_detail_enriched.csv")
-
-st.dataframe(df)
-
-
-
 
 import streamlit as st
 import pandas as pd
