ce5d3c7c4297b9e131f608873139d12a8f5c5ddf
ycshkim <26-3201@sc.gyo6.net>
2026-06-22 15:51:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -45,3 +45,29 @@ st.plotly_chart(
 )
 
 
+import streamlit as st
+import pandas as pd
+
+st.title("📊 문화재 현황")
+
+df = pd.read_csv("data/yc_heritage_detail_enriched.csv")
+
+st.dataframe(df)
+
+
+
+
+import streamlit as st
+import pandas as pd
+
+st.title("📍 문화재 위치")
+
+df = pd.read_csv("data/yc_heritage_detail_enriched.csv")
+
+map_df = df[["위도", "경도"]].dropna()
+map_df.columns = ["lat", "lon"]
+
+st.map(map_df)
+
+
+
